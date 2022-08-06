d5a93b788c5714ef3ed05848db476b039655a459
Saad <93708296+Cyber-Dioxide@users.noreply.github.com>
2022-08-06 20:52:06 +0500

    Update banner.py

--- scripts/banner.py
+++ scripts/banner.py
@@ -23,7 +23,7 @@ logo = f"""
   ███    ███   ███    ███ ███    ███     ███       ███    ███ 
 ▄█████████▀    ███    ███ ████████▀     ▄████▀     ██████████ 
                ███    ███                                     
-                          {c.c + "Author: "+c.y+"Saad Khan"}                                                                                                                                   
+                          {c.c + "Author: "+c.y+"Saad Khan | Cyber-Dioxide"}                                                                                                                                   
 """
 try:
     from colorama import Fore, Style
@@ -35,16 +35,15 @@ def banner():
     print(c.ran + logo)
     print(c.ran,"\n" + "|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX, "- " * 4, " [+] Follow me on Instagram @cyber_dioxide ", "- " * 4 + c.ran + "|")
     print(c.ran, "\n" + "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX, "- " * 4, " [+] Follow me on Instagram @coding_memz ", "- " * 4+c.ran + "|")
-    print(c.ran , "\n"+ "|"+ Style.BRIGHT + Fore.LIGHTRED_EX, "- " * 4, "[+] Github: https://github.com/Cyber-Dioxide/Stock-Termux ", "- " * 3+c.ran + "|")
+    print(c.ran , "\n"+ "|"+ Style.BRIGHT + Fore.LIGHTRED_EX, "- " * 4, "[+] Github: https://github.com/Cyber-Dioxide ", "- " * 3+c.ran + "|\n")
 
-    print(c.ran + '-' * 63)
 
 
 def banner2():
     print(c.ran + '-'*63)
     print(c.ran,"\n" + "|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX, "- " * 4, " [+] Follow me on Instagram @cyber_dioxide ", "- " * 4 + c.ran + "|")
     print(c.ran, "\n" + "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX, "- " * 4, " [+] Follow me on Instagram @coding_memz ", "- " * 4+c.ran + "|")
-    print(c.ran , "\n"+ "|"+ Style.BRIGHT + Fore.LIGHTRED_EX, "- " * 4, "[+] Github: https://github.com/Cyber-Dioxide/Stock-Termux ", "- " * 3+c.ran + "|")
+    print(c.ran , "\n"+ "|"+ Style.BRIGHT + Fore.LIGHTRED_EX, "- " * 4, "[+] Github: https://github.com/Cyber-Dioxide ", "- " * 3+c.ran + "|")
 
 def clear():
     s = platform.platform()
